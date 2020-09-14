d4c25a317d0e051c93f3749919842418a318da8d
Leon Sczepansky <leon.sczepansky@gmail.com>
2020-09-14 12:30:50 +0800

    counter shows one time

---
+++ frontend/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onMount } from 'svelte';
+	let counter = [];
+
+	onMount(async function() {
+        const response = await fetch('http://localhost:8000');
+		counter = await response.json();
+    });
+	
+
+	// onMount(() => {
+	// 	const interval = setInterval(() => {
+	// 		counter = fetchCounter();
+	// 	}, 1000);
+
+	// return () => {
+	// 		clearInterval(interval);
+	// 	};
+	// });
+
+</script>
+
+<main>
+	<p>Uhm-Counter: {counter.counter}</p>
+</main>
+
+<style>
+	p {
+		color: red;
+	}
+</style>
